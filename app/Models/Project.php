c05230361fdff05a3b86210f1679585536449cb9
Alun Price <alunprice089@gmail.com>
2024-04-25 12:03:26 +0100

    Built out the Projects table with Pagination, colour-coded statuses and edit / delete

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -13,4 +13,14 @@ class Project extends Model
     {
         return $this->hasMany(Task::class);
     }
-}
+
+    public function createdBy()
+    {
+      return $this->belongsTo(User::class, 'created_by'); // Assuming 'created_by' is the foreign key in the 'projects' table
+    }
+
+    public function updatedBy()
+    {
+      return $this->belongsTo(User::class, 'updated_by'); // Assuming 'updated_by' is the foreign key in the 'projects' table
+    }
+  }
